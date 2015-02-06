c41a9053268b15e80d7d661c6d585d6dd95f31ef
Barzin <barzin.m@gmail.com>
2015-02-06 00:50:37 -0500

    playing with Beautiful soup and csv

--- test_code.py
+++ test_code.py
@@ -1,13 +1,14 @@
 # test code
 
-import csv
-import StringIO
+import numpy as np
+import datetime as dt
 
 
+def mkdate(text):
+    return dt.datetime.strptime(text, '%Y-%m-%d')
 
-with open('City_Zhvi_Summary_AllHomes.csv','rb') as csvfile:
-    reader = csv.DictReader(csvfile)
-    header = reader.next()
-    print header
-    # for row in reader:
-	   #  print(row['Date'])
+database = np.genfromtxt('City_Zhvi_Summary_AllHomes.csv', delimiter=',', names=True, usecols=(0, 1), dtype=None, converters={'Date': mkdate})
+print database
+
+
+# http://stackoverflow.com/questions/7500864/python-array-of-datetime-objects-from-numpy-ndarray
